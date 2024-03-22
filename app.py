37ed1786f42c28717e87f7993eba7fe9dd915d5f
David D <142643241+abunchoftigers@users.noreply.github.com>
2024-03-21 20:26:45 -0500

    W3-C1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import io
 import streamlit as st
 import pandas as pd
 df = pd.read_csv('data/sales_predictions_2023_preprocessed.csv')
@@ -12,18 +13,22 @@ st.header("Product Sales Data")
 # Display the dataframe
 st.dataframe(df)
 
+
+# Buttons
 if st.button("Descriptive Statistics"):
   descriptive_stats = df.describe()
   st.dataframe(descriptive_stats)
 
+
 if st.button("Summary Information"):
-  summary_info = df.info()
-  st.write("Summary Information:")
-  st.dataframe(summary_info)
+  buffer = io.StringIO()
+  df.info(buf=buffer)
+  summary_info = buffer.getvalue()
+  st.text(summary_info)
+
 
 if st.button("Null Values"):
   null_values = df.isnull().sum()
-  st.write("Null Values:")
   st.write(null_values)
 
 
