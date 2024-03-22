f5d6e78683d7b396d7661ec68dfc666835b6c0dd
David D <142643241+abunchoftigers@users.noreply.github.com>
2024-03-21 21:35:41 -0500

    W3-C2

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# =======================================================================
+# PART A
+# =======================================================================
+
 import io
 import streamlit as st
 import pandas as pd
@@ -13,7 +17,6 @@ st.header("Product Sales Data")
 # Display the dataframe
 st.dataframe(df)
 
-
 # Buttons
 if st.button("Descriptive Statistics"):
   descriptive_stats = df.describe()
@@ -27,8 +30,32 @@ if st.button("Summary Information"):
   st.text(summary_info)
 
 
+import matplotlib.pyplot as plt
+
 if st.button("Null Values"):
   null_values = df.isnull().sum()
   st.write(null_values)
 
+# Explore Column Plots
+column_to_explore = st.selectbox("Select a column to explore", df.columns)
+if column_to_explore:
+  if df[column_to_explore].dtype == "object":
+    st.bar_chart(df[column_to_explore].value_counts())
+  else:
+    fig, ax = plt.subplots()
+    ax.scatter([1, 2, 3], [1, 2, 3])
+    plt.hist(df[column_to_explore])
+    st.pyplot(fig)
 
+# Feature vs Target Plots
+feature_to_explore = st.selectbox("Select a feature to explore", df.columns[:-1])
+
+if feature_to_explore:
+  if df[feature_to_explore].dtype == "object":
+    st.bar_chart(df.groupby(feature_to_explore)["Item_Outlet_Sales"].mean())
+  else:
+    fig, ax = plt.subplots()
+    ax.scatter([1, 2, 3], [1, 2, 3])
+    plt.hist(df[column_to_explore])
+    plt.scatter(df[feature_to_explore], df["Item_Outlet_Sales"])
+    st.pyplot(fig)
